828f3a2c921ef4136e4f2d60bb7b9921d118eabd
Thomas Jung <thomasjunginbox@gmail.com>
2016-12-03 14:17:54 -0800

    Get next page of result

--- lib/youtube/search.rb
+++ lib/youtube/search.rb
@@ -3,14 +3,22 @@ require "httparty"
 
 module YouTube
   class Search
-    # Your code goes here...
     def initialize(search_word)
       @search_word = search_word
     end
 
     def result
       response = HTTParty.get("https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + @search_word + "&key=#{ENV['API_KEY']}")
-      JSON.parse response.body
+      @result = JSON.parse response.body
+    end
+
+    def next_page_token
+      @result["nextPageToken"]
+    end
+
+    def next_page
+      response = HTTParty.get("https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + @search_word + "&key=#{ENV['API_KEY']}" + "&pageToken=#{next_page_token}")
+      @result = JSON.parse response.body
     end
   end
 end
